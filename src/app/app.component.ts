21dff994d13031d84c335b56c8c357ad78c7edc4
Mario <mariops3gow3@hotmail.com>
2020-02-18 12:27:44 -0600

    pruebas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { summaryFileName } from '@angular/compiler/src/aot/util';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'UI-Angular';
+  titulo = 'UI-Angular';
+  edad: number;
+  private dumasValores: number = 0;
+  private num1: number = 0;
+  private num2: number = 0;
+  private aux: number = 0;
+  
+
+public suma(aux: number, aux2: number): number{
+  
+  return  aux + aux2;  
+
+}
+  
+
 }
